31be3999b8dd21042d07b66abdce277cf232ce63
sapardi <sapardigo@gmail.com>
2025-09-17 22:50:39 +0700

    user icon di sembunyikan dulu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { MdMenu, MdClose, MdAccountCircle } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";  // , MdAccountCircle <- dikeluarkan dari dalam {}
 import "./NavBar.css";
 
 export default function NavBar() {
@@ -32,7 +32,7 @@ export default function NavBar() {
 
       {/* User + Hamburger */}
       <div className="actions">
-        <MdAccountCircle className="icon user" />
+        {/* <MdAccountCircle className="icon user" /> */}
         <button
           className="icon menu-toggle"
           onClick={() => setIsOpen(!isOpen)}
