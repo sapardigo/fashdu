614a466ebb5a59259f2eec40bf8418749ef5671e
sapardi <sapardigo@gmail.com>
2025-09-20 10:23:51 +0700

    mensederhanakan beberapa hal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ export default async function Home() {
     <div className="main-heading">
       <h2>FASHDU - Sehatkan Tubuhmu!</h2>
       <p className="subtitle">FASHDU adalah tehnik mengeluarkan darah dari pembuluh darah vena, yang di dalamnya terdapat sumbatan-sumbatan yang merugikan tubuh, dengan cara membuka pemukaan kulit, dengan alat yang hygienis steril dan aman, sehingga darah yang menyumbat terdorong keluar</p>
+      <h3>Manfaat Fashdu</h3>
+      <p className="subtitle">FASHDU adalah tehnik mengeluarkan darah dari pembuluh darah vena, yang di dalamnya terdapat sumbatan-sumbatan yang merugikan tubuh, dengan cara membuka pemukaan kulit, dengan alat yang hygienis steril dan aman, sehingga darah yang menyumbat terdorong keluar</p>
+    <p className="subtitle">FASHDU adalah tehnik mengeluarkan darah dari pembuluh darah vena, yang di dalamnya terdapat sumbatan-sumbatan yang merugikan tubuh, dengan cara membuka pemukaan kulit, dengan alat yang hygienis steril dan aman, sehingga darah yang menyumbat terdorong keluar</p>
+    <p className="subtitle">FASHDU adalah tehnik mengeluarkan darah dari pembuluh darah vena, yang di dalamnya terdapat sumbatan-sumbatan yang merugikan tubuh, dengan cara membuka pemukaan kulit, dengan alat yang hygienis steril dan aman, sehingga darah yang menyumbat terdorong keluar</p>
+    <p className="subtitle">FASHDU adalah tehnik mengeluarkan darah dari pembuluh darah vena, yang di dalamnya terdapat sumbatan-sumbatan yang merugikan tubuh, dengan cara membuka pemukaan kulit, dengan alat yang hygienis steril dan aman, sehingga darah yang menyumbat terdorong keluar</p>
+    
     </div>
     
     </>
